750bb2c1bde2723ad27487610e2e7526675b805d
Suki Sahota <ssahota@berkeley.edu>
2019-04-09 11:53:04 -0700

    added get routes

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 const express = require("express");
 
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+// Tells node that we are creating an "express" server
 const app = express();
 
 // Set the port of our application
@@ -35,7 +45,19 @@ connection.connect(function(err) {
 });
 
 
-// Start our server so that it can begin listening to client requests.
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
