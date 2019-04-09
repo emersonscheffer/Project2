b3b4b5c3334712773994c5d47729059da73e3480
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-04-09 12:57:37 -0700

    changed the server

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "Nicolas2017",
   database: "warehouse_db"
 });
 
