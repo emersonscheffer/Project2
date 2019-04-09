92e7a48ba9d47f5fb163905647ab6f8cd7736c86
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-04-09 12:40:09 -0700

    selection on kh

--- app/public/kitchenhub.js
+++ app/public/kitchenhub.js
@@ -13,12 +13,20 @@ for (let index = 0; index < 16; index++) {
     slots.style.width = 100 / 4 + "%";
     slots.style.height = 100 / 4 + "%";
     slots.appendChild(document.createElement("p"));
+
+    slots.setAttribute("onmouseenter", "hovering(event)");
+    slots.setAttribute("onmouseleave", "hoverout(event)");
+    slots.setAttribute("onclick", "selecting(event)");
+    slots.setAttribute("data-selected", false);
+    slots.setAttribute("data-isselectable", true);
+
+
     slots.lastElementChild.textContent = "Square " + (index + 1);
     map.appendChild(slots);
 
 }
 // get values from form
-let submitBtn = document.getElementById("submit-btn").onclick = function(event){
+let submitBtn = document.getElementById("submit-btn").onclick = function (event) {
     event.preventDefault();
 
     var kName = document.getElementById('kname').value.trim();
@@ -26,7 +34,7 @@ let submitBtn = document.getElementById("submit-btn").onclick = function(event){
     var kWeb = document.getElementById('kweb').value.trim();
     var kTel = document.getElementById('ktel').value.trim();
     var kDesc = document.getElementById('description').value.trim();
-    
+
     console.log(kName);
     console.log(kType);
     console.log(kWeb);
@@ -34,3 +42,61 @@ let submitBtn = document.getElementById("submit-btn").onclick = function(event){
     console.log(kDesc);
 };
 
+function hovering(event) {
+    if (event.target.dataset.selected === "true") {
+        ;
+    } else {
+        event.target.style.border = "solid 3px rgb(120 120 255)";
+    }
+
+}
+
+function hoverout(event) {
+    event.target.style.border = "solid 3px #ffe18f";
+}
+
+function selecting(event) {
+
+    var allselected = document.querySelectorAll(".slots");
+
+    if (event.target.dataset.selected === "true") {
+        //if (event.target.dataset.isselectable === "false") {
+            event.target.style.backgroundColor = "#929292";
+            event.target.dataset.selected = false;
+            //event.target.dataset.isselectable = false;
+
+            allselected.forEach(function (item) {
+                item.dataset.selected = false;
+                item.dataset.isselectable = true;
+            });
+
+        //}
+    } else if (event.target.dataset.selected === "false") {
+        if (event.target.dataset.isselectable === "true") {
+            event.target.style.backgroundColor = "#cacaca";
+            event.target.dataset.selected = true;
+
+            allselected.forEach(function (item) {
+                item.dataset.selected = true;
+                item.dataset.isselectable = false;
+            });
+
+            event.target.dataset.isselectable = true;
+        }
+
+    }
+
+
+
+
+
+
+
+
+    // var highlightedItems = userList.querySelectorAll(".highlighted");
+
+    // highlightedItems.forEach(function (userItem) {
+    //     deleteUser(userItem);
+    // });
+
+}
